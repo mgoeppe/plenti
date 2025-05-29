680049c4e82aabdf145d4a4f72d11e98bd8de64e
mgoeppe <mathias.goeppel@googlemail.com>
2025-05-29 23:08:22 +0200

    parse fields as own func

--- cmd/data.go
+++ cmd/data.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/matoubidou/plenticore-to-db/pkg/plenticore"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -14,44 +15,43 @@ var dataCmd = &cobra.Command{
 	Short: "Get data from Plenticore inverter",
 	Long:  `Retrieve current data from the Plenticore inverter and optionally save it to a database.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		m2f := make(map[string][]string)
-		for _, field := range viper.GetStringSlice("plenticore.fields") {
-			p := strings.SplitN(field, "/", 2)
-			if len(p) != 2 {
-				cmd.PrintErrf("Invalid field format: %s. Expected format is 'moduleID/fieldID'\n", field)
-				return
-			}
-			moduleID, fieldID := p[0], p[1]
-			m2f[moduleID] = append(m2f[moduleID], fieldID)
-		}
-		fields := make([]plenticore.Fields, 0, len(m2f))
-		for moduleID, fieldIDs := range m2f {
-			fields = append(fields, plenticore.Fields{
-				ModuleID: moduleID,
-				FieldIDs: fieldIDs,
-			})
-		}
-		if len(fields) == 0 {
-			fields = commandCtx.Client.Fields()
-		}
-
-		// Sort fields by moduleID and fieldIDs alphanumerically
-		sort.Slice(fields, func(i, j int) bool {
-			return fields[i].ModuleID < fields[j].ModuleID
-		})
-
-		// Sort each field's FieldIDs slice
-		for i := range fields {
-			sort.Strings(fields[i].FieldIDs)
-		}
-
-		data := commandCtx.Client.Data(fields)
+		data := commandCtx.Client.Data(ParseFields())
 		for _, module := range data {
 			for _, field := range module.Fields {
 				cmd.Printf("%s/%s: %f %s\n", module.ModuleID, field.ID, field.Value, field.Unit)
 			}
 		}
 	},
+}
+
+func ParseFields() []plenticore.Fields {
+	m2f := make(map[string][]string)
+	for _, field := range viper.GetStringSlice("plenticore.fields") {
+		p := strings.SplitN(field, "/", 2)
+		if len(p) != 2 {
+			logrus.Fatal("Invalid field format: %s. Expected format is 'moduleID/fieldID'\n", field)
+		}
+		moduleID, fieldID := p[0], p[1]
+		m2f[moduleID] = append(m2f[moduleID], fieldID)
+	}
+	fields := make([]plenticore.Fields, 0, len(m2f))
+	for moduleID, fieldIDs := range m2f {
+		fields = append(fields, plenticore.Fields{
+			ModuleID: moduleID,
+			FieldIDs: fieldIDs,
+		})
+	}
+	if len(fields) == 0 {
+		fields = commandCtx.Client.Fields()
+	}
+
+	sort.Slice(fields, func(i, j int) bool {
+		return fields[i].ModuleID < fields[j].ModuleID
+	})
+	for i := range fields {
+		sort.Strings(fields[i].FieldIDs)
+	}
+	return fields
 }
 
 func init() {
